d659598beacf3872b3d914b283307a440cff8d01
Abduh Riski <90995388+abduhriski@users.noreply.github.com>
2023-07-31 08:21:03 +0700

    stream_app.py

--- app.py
+++ app.py
@@ -1,3 +1,18 @@
+import streamlit as st
+
+if not hasattr(st, 'already_started_server'):
+    # Hack the fact that Python modules (like st) only load once to
+    # keep track of whether this file already ran.
+    st.already_started_server = True
+
+    st.write('''
+        The first time this script executes it will run forever because it's
+        running a Flask server.
+
+        Just close this browser tab and open a new one to see your Streamlit
+        app.
+    ''')
+
 from flask import Flask, request, render_template
 import pickle
 
